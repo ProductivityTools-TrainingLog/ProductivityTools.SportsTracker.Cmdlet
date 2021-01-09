5ba9c36f3c64f01dedfa577d4f9c5ce11b5b070c
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-09 18:21:36 +0100

    first cmdlet

---
+++ ProductivityTools.SportsTracker.App/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProductivityTools.SportsTracker.App
+{
+    public class Class1
+    {
+    }
+}
---
+++ ProductivityTools.SportsTracker/ImportTraining/ImportTrainingCmdlet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace ProductivityTools.SportsTracker.ImportTraining
+{
+    [Cmdlet("Import", "Training")]
+    public class ImportTrainingCmdlet : PSCmdlet.PSCmdletPT
+    {
+        protected override void ProcessRecord()
+        {
+            WriteOutput("Hello");
+            base.ProcessRecord();
+        }
+    }
+}
