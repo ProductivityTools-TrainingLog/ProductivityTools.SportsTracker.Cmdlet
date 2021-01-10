e5cf9de5b2e5f2001f972e53b9107a8a4f1cb533
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-10 19:50:25 +0100

    minutes

--- ProductivityTools.SportsTracker/AddTraining/AddTrainingCmdlet.cs
+++ ProductivityTools.SportsTracker/AddTraining/AddTrainingCmdlet.cs
@@ -8,8 +8,12 @@ namespace ProductivityTools.SportsTracker.AddTraining
     [Cmdlet(VerbsCommon.Add, "Training")]
     public class AddTrainingCmdlet : STCmdlet
     {
+        [Parameter]
+        public int Minutes { get; set; }
+
         protected override void ProcessRecord()
         {
+            int time = Minutes * 60;
             base.Application.AddTraining(App.Dto.TrainingType.Areobics, "nothing", 120, DateTime.Now);
             base.ProcessRecord();
         }
