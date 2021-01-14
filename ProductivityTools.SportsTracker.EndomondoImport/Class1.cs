37661af74abf0ccb8b377c07865525cefec079dd
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-01-14 22:53:30 +0100

    endomondo import

---
+++ ProductivityTools.SportsTracker.EndomondoImport/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProductivityTools.SportsTracker.EndomondoImport
+{
+    public class EndomondoImport
+    {
+        public string Path { get; set; }
+
+        public EndomondoImport(string path)
+        {
+            this.Path = path;
+        }
+
+        public void Import()
+        {
+
+        }
+    }
+}
